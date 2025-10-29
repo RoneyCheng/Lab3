a36ed560be0bc9744b3c5827d7c1499c64403f61
RoneyCheng <WAILIN.24@ichat.sp.edu.sg>
2025-10-29 21:32:21 +0800

    A python file that is used to test the output

---
+++ Test_bmi.py
@@ -0,0 +1,13 @@
+from Lab2 import bmi
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.70, 60)   
+    assert(result == 0)
+
+def test_bmi_over_weight(): 
+    result = bmi.calculate_bmi(1.70,75)   
+    assert(result == 1)
+
+def test_bmi_under_weight(): 
+    result = bmi.calculate_bmi(1.70,45)
+    assert(result == -1)
+
